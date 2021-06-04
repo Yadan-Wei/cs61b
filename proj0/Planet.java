f5647f691a3819da332013ddbec39b50d2c4ca49
Yadan-Wei <yadanwei0712@gmail.com>
2021-06-04 16:35:33 -0700

    Complete part of project 1

---
+++ proj0/Planet.java
@@ -0,0 +1,86 @@
+public class Planet{
+
+	public double xxPos;
+	public double yyPos;
+	public double xxVel;
+	public double yyVel;
+	public double mass;
+	public String imgFileName;
+
+	/**Declaring variables only as static can lead to change in their values 
+	 * by one or more instances of a class in which it is declared.
+	Declaring them as static final will help you to create a CONSTANT. 
+	Only one copy of variable exists which can’t be reinitialize.*/
+
+	final static double gravConstant = 6.67e-11;
+
+
+	public Planet(double xP, double yP, double xV, double yV,
+		double m, String img){
+		xxPos = xP;
+		yyPos = yP;
+		xxVel = xV;
+		yyVel = yV;
+		mass = m;
+		imgFileName = img;
+	}
+
+	public Planet(Planet p){
+		xxPos = p.xxPos;
+		yyPos = p.yyPos;
+		xxVel = p.xxVel;
+		yyVel = p.yyVel;
+		mass = p.mass;
+		imgFileName = p.imgFileName;
+	}
+
+	public double calcDistance(Planet p){
+		double xDis = this.xxPos - p.xxPos;
+		double yDis = this.yyPos - p.yyPos;
+		return Math.sqrt(xDis*xDis + yDis*yDis);
+	}
+
+	public double calcForceExertedBy(Planet p){
+		double dis = calcDistance(p);
+		double force = gravConstant*p.mass*this.mass/(dis*dis);
+		return force;
+	}
+
+	public double calcForceExertedByX(Planet p){
+		double xDis = p.xxPos - this.xxPos;
+		double force = calcForceExertedBy(p);
+		double dis = calcDistance(p);
+		double forceX = force*xDis/dis;
+		return forceX;
+	}
+
+	public double calcForceExertedByY(Planet p){
+		double yDis = p.yyPos - this.yyPos;
+		double force = calcForceExertedBy(p);
+		double dis = calcDistance(p);
+		double forceY = force*yDis/dis;
+		return forceY;
+	}
+
+	public double calcNetForceExertedByX(Planet[] planets) {
+		double netForceX = 0;
+		for (int i=0; i < planets.length; i ++){
+			if (this.equals(planets[i])){
+				continue;
+			}
+			netForceX = netForceX + calcForceExertedByX(planets[i]);
+		}
+		return netForceX;
+	}
+
+	public double calcNetForceExertedByY(Planet[] planets) {
+		double netForceY = 0;
+		for (Planet s: planets){
+			if (this.equals(s)){
+				continue;
+			}
+			netForceY = netForceY+ calcForceExertedByY(s);
+		}
+		return netForceY;
+	}
+}
