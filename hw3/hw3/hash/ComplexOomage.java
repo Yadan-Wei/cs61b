24a3398bfe50e368a44e093b9df0d2f4dfe0d33f
Yadan-Wei <yadanwei0712@gmail.com>
2021-08-31 16:13:47 -0700

    hw3

--- hw3/hw3/hash/ComplexOomage.java
+++ hw3/hw3/hash/ComplexOomage.java
@@ -13,6 +13,7 @@ public class ComplexOomage implements Oomage {
     public int hashCode() {
         int total = 0;
         for (int x : params) {
+            x = x/5;
             total = total * 256;
             total = total + x;
         }
--- hw3/hw3/hash/TestSimpleOomage.java
+++ hw3/hw3/hash/TestSimpleOomage.java
@@ -29,6 +29,15 @@ public class TestSimpleOomage {
           meaning no two SimpleOomages should EVER have the same
           hashCode UNLESS they have the same red, blue, and green values!
          */
+        SimpleOomage ooA = new SimpleOomage(5, 10, 20);
+        SimpleOomage ooA2 = new SimpleOomage(5, 10, 20);
+        SimpleOomage ooB = new SimpleOomage(10, 5, 20);
+        SimpleOomage ooC = new SimpleOomage(20, 10, 5);
+        SimpleOomage ooD = new SimpleOomage(5, 20, 10);
+        assertNotEquals(ooA.hashCode(),ooB.hashCode());
+        assertNotEquals(ooB.hashCode(),ooC.hashCode());
+        assertNotEquals(ooC.hashCode(),ooD.hashCode());
+        assertEquals(ooA.hashCode(),ooA2.hashCode());
     }
 
     @Test
@@ -42,7 +51,7 @@ public class TestSimpleOomage {
         assertNotEquals(ooA, "ketchup");
     }
 
-    /*
+
     @Test
     public void testHashCodeAndEqualsConsistency() {
         SimpleOomage ooA = new SimpleOomage(5, 10, 20);
@@ -50,10 +59,10 @@ public class TestSimpleOomage {
         HashSet<SimpleOomage> hashSet = new HashSet<>();
         hashSet.add(ooA);
         assertTrue(hashSet.contains(ooA2));
-    }*/
+    }
 
     /* TODO: Uncomment this test after you finish haveNiceHashCode Spread in OomageTestUtility */
-    /*@Test
+    @Test
     public void testRandomOomagesHashCodeSpread() {
         List<Oomage> oomages = new ArrayList<>();
         int N = 10000;
@@ -63,7 +72,7 @@ public class TestSimpleOomage {
         }
 
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(oomages, 10));
-    }*/
+    }
 
     /** Calls tests for SimpleOomage. */
     public static void main(String[] args) {
